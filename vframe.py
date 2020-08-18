2a3392f0354bf14179fd844e319ae37dbc364932
dhruva1108 <64674928+dhruva1108@users.noreply.github.com>
2020-08-18 12:03:17 +0530

    Delete vframe.py

--- vframe.py
+++
@@ -1,9 +0,0 @@
-import vaex
-import dask.dataframe as dd
-import modin.pandas as pd
-
-def vframe(a):
-    df_dask = dd.read_csv(a)
-    df_pandas = df_dask.compute()
-    df_vaex = vaex.from_pandas(df_pandas)
-    return df_vaex
